74f82ab9efcd560a0ffaaca141436b3c885f4527
niels1286 <niels@nuls.io>
2023-05-15 19:54:36 +0800

    fix update script

--- update-images.sh
+++ update-images.sh
@@ -1,10 +1,8 @@
 git pull
-docker rmi docker.nuls.io/repository/docker/contract_validation
-docker rmi contract_validation
 mvn clean -DskipTests=true package
 cd target && mkdir contract-validation-server && mv contract-validation-server.tar.gz contract-validation-server/
 cd contract-validation-server && tar -xzf contract-validation-server.tar.gz
 pwd
 ./make_image.sh
-docker tag contract_validation:latest docker.nuls.io/repository/docker/contract_validation:latest
-docker push docker.nuls.io/repository/docker/contract_validation:latest
+./stop_container.sh
+./start_container.sh
