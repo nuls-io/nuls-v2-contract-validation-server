768888ca49e3bfcd2baa4b9868602019ba370eee
niels1286 <niels@nuls.io>
2023-05-15 20:03:03 +0800

    fix update script

--- update-images.sh
+++
@@ -1,8 +0,0 @@
-git pull
-mvn clean -DskipTests=true package
-cd target && mkdir contract-validation-server && mv contract-validation-server.tar.gz contract-validation-server/
-cd contract-validation-server && tar -xzf contract-validation-server.tar.gz
-pwd
-./make_image.sh
-./stop_container.sh
-./start_container.sh
